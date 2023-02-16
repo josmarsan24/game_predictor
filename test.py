73859165cdbadc89c889fe7508e49478314f9f35
josmarsan24 <josmarsan24@alum.us.es>
2023-02-16 18:30:28 +0100

    actualizo la formula de prediccion

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 #read csv
-data = pd.read_csv("game_predictor/nba_data_norm.csv", header = 0)
+data = pd.read_csv("nba_data_norm.csv", header = 0)
 #data = pd.read_csv("nba_data.csv", header = 0)
 data.set_index("team_id")
 
@@ -12,8 +12,8 @@ def get_teams_by_id(home_id, away_id):
     return home_team, away_team
 
 def predict(home, away):
-    home_rtg = home['PTS'].values[0]*0.333 + home['opPTS'].values[0]*0.333 + home['AST'].values[0]*0.167 + home['TRB'].values[0]*0.167
-    away_rtg = away['PTS'].values[0]*0.333 + away['opPTS'].values[0]*0.333 + away['AST'].values[0]*0.167 + away['TRB'].values[0]*0.167
+    home_rtg = home['PTS'].values[0]*0.175 + home['PF'].values[0]*0.025 + home['TOV'].values[0]*0.05 + home['BLK'].values[0]*0.025 + home['STL'].values[0]*0.025 + home['AST'].values[0]*0.075 + home['TRB'].values[0]*0.075 + home['FG%'].values[0]*0.025 + home['3P%'].values[0]*0.025 + home['opPTS'].values[0]*0.175 + home['opPF'].values[0]*0.025 + home['opTOV'].values[0]*0.05 + home['opBLK'].values[0]*0.025 + home['opSTL'].values[0]*0.025 + home['opAST'].values[0]*0.075 + home['opTRB'].values[0]*0.075 + home['opFG%'].values[0]*0.025 + home['op3P%'].values[0]*0.025
+    away_rtg = away['PTS'].values[0]*0.175 + away['PF'].values[0]*0.025 + away['TOV'].values[0]*0.05 + away['BLK'].values[0]*0.025 + away['STL'].values[0]*0.025 + away['AST'].values[0]*0.075 + away['TRB'].values[0]*0.075 + away['FG%'].values[0]*0.025 + away['3P%'].values[0]*0.025 + away['opPTS'].values[0]*0.175 + away['opPF'].values[0]*0.025 + away['opTOV'].values[0]*0.05 + away['opBLK'].values[0]*0.025 + away['opSTL'].values[0]*0.025 + away['opAST'].values[0]*0.075 + away['opTRB'].values[0]*0.075 + away['opFG%'].values[0]*0.025 + away['op3P%'].values[0]*0.025
     home_odds = 100 * home_rtg / (home_rtg + away_rtg)
     away_odds = 100 * away_rtg / (home_rtg + away_rtg)
     return home_odds, away_odds
