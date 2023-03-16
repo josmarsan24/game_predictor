e211939eab1f7efaf0978080d31ff28650effd23
josmarsan24 <josmarsan24@alum.us.es>
2023-03-16 11:09:41 +0100

    añado la tabla y el scrpit para crear la tabla del h2h

--- write_data.py
+++ write_data.py
@@ -26,4 +26,61 @@ def home_adv():
     df['away_W/L'] = 1 - df['home_W/L']
     df.to_csv('win_rate.csv', index=False, header=True)
 
-home_adv()
+def h2h():
+    df = pd.DataFrame(columns = ['team_id','home_team','Atlanta Hawks',
+        'Boston Celtics',
+        'Brooklyn Nets',
+        'Charlotte Hornets',
+        'Chicago Bulls',
+        'Cleveland Cavaliers',
+        'Dallas Mavericks',
+        'Denver Nuggets',
+        'Detroit Pistons',
+        'Golden State Warriors',
+        'Houston Rockets',
+        'Indiana Pacers',
+        'Los Angeles Clippers',
+        'Los Angeles Lakers',
+        'Memphis Grizzlies',
+        'Miami Heat',
+        'Milwaukee Bucks',
+        'Minnesota Timberwolves',
+        'New Orleans Pelicans',
+        'New York Knicks',
+        'Oklahoma City Thunder',
+        'Orlando Magic',
+        'Philadelphia 76ers',
+        'Phoenix Suns',
+        'Portland Trail Blazers',
+        'Sacramento Kings',
+        'San Antonio Spurs',
+        'Toronto Raptors',
+        'Utah Jazz',
+        'Washington Wizards'])
+    df['home_team'] = wr['Row.names']
+    df['team_id'] = wr['team_id']
+    df = df.replace(np.nan, '0-0')
+
+    
+    for index, row in games.iterrows():
+        home_name = games['Home/Neutral'].values[index]
+        away_name = games['Visitor/Neutral'].values[index]
+        if (row['away_PTS'] < row['home_PTS']):
+            df.loc[df['home_team'] == home_name,away_name] = add_vict(df.loc[df['home_team'] == home_name,away_name].values[0],True)
+        else:
+            df.loc[df['home_team'] == home_name,away_name] = add_vict(df.loc[df['home_team'] == home_name,away_name].values[0],False)
+    df.to_csv('h2h.csv', index=False, header=True)
+
+def add_vict(record, home):
+    try:
+        if home==True:
+            record = str(int(record.split('-')[0]) + 1) + '-' + record.split('-')[1]
+        else:
+            record = record.split('-')[0] + '-' + str(int(record.split('-')[1]) + 1)
+        return record
+    except:
+        return '0-0'
+
+
+h2h()
+#home_adv()
